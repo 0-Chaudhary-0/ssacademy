6aeb66cd8c29bd03ea57edc1bf7a480ef6e948fc
chaudhary <azeemshahid565@gmail.com>
2024-04-13 08:33:36 +0100

    try to fix database

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -36,7 +36,7 @@ const login = () => {
   let handleSubmit = async (event) => {
     event.preventDefault();
     setProgress(30);
-    const response = await fetch("http://localhost:3000/api/auth/login", {
+    const response = await fetch("https://ssacademy-rust.vercel.app/api/auth/login", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
