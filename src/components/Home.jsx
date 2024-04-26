e3ab540828ba2f7298c4cd11ed5a87a5fcd19e65
chaudhary <azeemshahid565@gmail.com>
2024-04-26 07:06:23 +0100

    added image

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -135,7 +135,7 @@ const Professors = () => {
           <div className="flex-col px-6 py-2 ">
             <div className="h-40 w-60 rounded-2xl overflow-hidden bg-gray-200">
               <img
-                src="https://source.unsplash.com/300x300/?study"
+                src="/asif.jpg"
                 alt=""
                 className=""
               />
